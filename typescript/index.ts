89cb0dad87c1751709e2a0b2d71e5934c1bcef98
peter <1612750269@qq.com>
2018-12-24 10:54:26 +0800

    'fix'

---
+++ typescript/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by peter on 2018/12/18.
+ */
+export class Hello {
+
+}
---
+++ typescript/test.js
@@ -0,0 +1,111 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+/**
+ * Created by peter on 2018/12/18.
+ */
+//todo 经典js
+/*
+function Greeter(greeting) {
+    this.greeting = greeting;
+}
+
+Greeter.prototype.greet = function() {
+    return "Hello, " + this.greeting;
+}
+
+// Oops, we're passing an object when we want a string. This will print
+// "Hello, [object Object]" instead of "Hello, world" without error.
+let greeter = new Greeter({message: "world"});
+
+let button = document.createElement('button');
+button.textContent = "Say Hello";
+button.onclick = function() {
+    alert(greeter.greet());
+};
+
+document.body.appendChild(button);*/
+//TODO 添加类型
+/*function Greeter(greeting: string) {
+    this.greeting = greeting;
+}
+
+Greeter.prototype.greet = function() {
+    return "Hello, " + this.greeting;
+}
+
+let greeter = new Greeter("world");
+
+let button = document.createElement('button');
+button.textContent = "Say Hello";
+button.onclick = function() {
+    alert(greeter.greet());
+};
+
+document.body.appendChild(button);*/
+//TODO 使用类
+/*class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+let greeter = new Greeter("world");
+
+let button = document.createElement('button');
+button.textContent = "Say Hello";
+button.onclick = function() {
+    alert(greeter.greet());
+}
+
+document.body.appendChild(button);*/
+//todo 使用继承
+var Animal = (function () {
+    function Animal(name) {
+        this.name = name;
+    }
+    Animal.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 0; }
+        console.log(this.name + " moved " + distanceInMeters + "m.");
+    };
+    return Animal;
+}());
+var Snake = (function (_super) {
+    __extends(Snake, _super);
+    function Snake(name) {
+        return _super.call(this, name) || this;
+    }
+    Snake.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 5; }
+        console.log("Slithering...");
+        _super.prototype.move.call(this, distanceInMeters);
+    };
+    return Snake;
+}(Animal));
+var Horse = (function (_super) {
+    __extends(Horse, _super);
+    function Horse(name) {
+        return _super.call(this, name) || this;
+    }
+    Horse.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 45; }
+        console.log("Galloping...");
+        _super.prototype.move.call(this, distanceInMeters);
+    };
+    return Horse;
+}(Animal));
+var sam = new Snake("Sammy the Python");
+var tom = new Horse("Tommy the Palomino");
+sam.move();
+tom.move(34);
