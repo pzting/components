042c69b9a92fd4e49981198ba6f262741ecff6cd
peter <1612750269@qq.com>
2019-01-03 16:46:58 +0800

    聊天

---
+++ webSocket/server.js
@@ -0,0 +1,20 @@
+var http=require("http");//引入一个模块
+var fs=require('fs');//引入文件系统模块
+var ws=require('socket.io');//
+
+var server=http.createServer(function(req,res){//请求，发送
+	var html=fs.readFileSync("./chat.html");//
+	res.end(html);
+}).listen(3000);//创建服务器
+
+var io=ws(server);//websocket服务器
+
+io.on('connection',function(socket){//socket可以看成客户端
+	
+	console.log('进来了')
+	
+	socket.on('message',function(obj){//obj 所有已经建立连接的对象
+		console.log(obj)
+		io.emit('message',obj);//发送消息
+	})
+});//监听事件
