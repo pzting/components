bccf1de7a7e119f55117d021e901d281896ac9af
peter <1612750269@qq.com>
2019-01-09 15:57:13 +0800

    第一版，正常聊天

--- webSocket/server.js
+++ webSocket/server.js
@@ -1,13 +1,36 @@
 var http=require("http");//引入一个模块
 var fs=require('fs');//引入文件系统模块
 var ws=require('socket.io');//
-const path = require('path')
+// const path = require('path')
 
-var server=http.createServer(function(req,res){//请求，发送
-	var html=fs.readFileSync("./chat.html");//
-	res.end(html);
-	var css=fs.readFileSync(path.resolve(__dirname, 'web'));//
-	res.end(css);
+var server=http.createServer(function(request,respone){//请求，发送
+	//var html=fs.readFileSync("./chat.html");//
+	console.log(request.url,'====')
+    var url = request.url
+    if(url=='/'){
+        respone.writeHead(200,{'Content-Type':'text/html'});
+        var ns = fs.readFile('./chat.html', function(err, data) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            respone.end(data);
+        });
+        //respone.end();
+    }else if(url != '/'){
+        var surl = '.'+url;
+        var type = surl.substr(surl.lastIndexOf(".")+1,surl.length)
+        respone.writeHead(200,{'Content-type':"text/"+type});
+        // respone.writeHead(200,{'Content-Type':'text/css'});
+        var ns = fs.readFile(surl, function(err, data) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            respone.end(data);
+        });
+    }
+	console.log(request.url,'====')
 }).listen(3000);//创建服务器
 
 var io=ws(server);//websocket服务器
