bfb61453666aefb3482a3dfe262354618e0e82d9
peter <1612750269@qq.com>
2019-01-11 11:10:36 +0800

    输入中2版

--- webSocket/js/chat.js
+++ webSocket/js/chat.js
@@ -10,7 +10,7 @@ var chatContent = document.getElementById('chatContent');
 let chatObj = {
     userId: '',
     headImg: '',
-    wordArr: ['鼠','牛','虎','兔','龙','蛇','马','羊','猴','鸡','狗','猪'],
+    wordArr: ['鼠', '牛', '虎', '兔', '龙', '蛇', '马', '羊', '猴', '鸡', '狗', '猪'],
     bg: '',
     word: '',
     lastDate: null,
@@ -19,8 +19,8 @@ let chatObj = {
     basic () { // 默认值
         this.userId = Math.random().toString()
 //            this.headImg = parseInt(Math.random()*2)+1
-        this.bg = this.bgColor(255,255,255,.8);
-        this.word = this.wordArr[parseInt(Math.random()*12)]
+        this.bg = this.bgColor(255, 255, 255, .8);
+        this.word = this.wordArr[parseInt(Math.random() * 12)]
         console.log(this.word)
     },
     init () { // 初始化
@@ -28,33 +28,38 @@ let chatObj = {
         this.send()
         this.accept()
     },
-    bgColor (r,g,b,a) { // 随机生成颜色
-        return 'rgba('+parseInt(Math.random()*r)+','+parseInt(Math.random()*g)+','+parseInt(Math.random()*b)+','+a+')'
+    bgColor (r, g, b, a) { // 随机生成颜色
+        return 'rgba(' + parseInt(Math.random() * r) + ',' + parseInt(Math.random() * g) + ',' + parseInt(Math.random() * b) + ',' + a + ')'
     },
     time () {
         var date = new Date();
         var year = date.getFullYear()
-        var month = date.getMonth()+1
+        var month = date.getMonth() + 1
         var hours = date.getHours(),
             minutes = date.getMinutes();
-        return year+'-'+month+' '+this.timeFormat(hours)+ ':'+this.timeFormat(minutes);
+        return year + '-' + month + ' ' + this.timeFormat(hours) + ':' + this.timeFormat(minutes);
     },
     timeFormat (n) {
-        return n<10?'0'+n:n;
+        return n < 10 ? '0' + n : n;
     },
     send () { // 发送消息
         let me = this
-        document.onkeydown = function (event) {
+        document.onkeyup = function (event) {
             let nowDate = new Date()
-            let TDOA = nowDate-me.lastDate
-            me.isShowTime = (TDOA>1000*60*10) ? true : false
+            let TDOA = nowDate - me.lastDate
+            me.isShowTime = (TDOA > 1000 * 60 * 10) ? true : false
             var e = event || window.event;
-            var obj ={
-                text: '发送中...',
+            if (chatBox.value) {
+                me.isInput = true
+            } else {
+                me.isInput = false
+            }
+            var obj = {
+                text: '输入中...',
                 userId: me.userId,
                 bg: me.bg,
                 word: me.word,
-                isInput: true,
+                isInput: me.isInput,
                 sending: true,
                 isShowTime: me.isShowTime,
                 headImg: me.headImg
@@ -64,12 +69,19 @@ let chatObj = {
             }
             socket.send(obj)
             /*if (!me.isInput){
-                socket.send(obj)
-                me.isInput = true
-            }*/
+             socket.send(obj)
+             me.isInput = true
+             }*/
             if (e && e.keyCode == 13) { //回车键的键值为13
+                if (chatBox == document.activeElement) {
+                    // alert('获取焦点');
+                } else {
+                    chatBox.focus()
+                    return
+                }
+                if (!chatBox.value) return
                 me.isInput = false;
-                var obj ={
+                var obj = {
                     text: chatBox.value,
                     userId: me.userId,
                     bg: me.bg,
@@ -94,41 +106,45 @@ let chatObj = {
             div.className = 'chat chatLeft';
             // 先删除掉 类是sending的元素
             document.querySelector('.sending') && (chatContent.removeChild(document.querySelector('.sending')))
-            if (mes.sending){ // 判断发送的状态
+            if (mes.sending) { // 判断发送的状态
                 div.className = 'chat sending chatLeft';
             }
-            if(mes.isShowTime){
-                divTime.innerHTML = '<span>'+me.time()+'</span>'
+            if (mes.isShowTime) {
+                divTime.innerHTML = '<span>' + me.time() + '</span>'
                 chatContent.appendChild(divTime)
             }
             var html = ''
-            if (me.userId != mes.userId){ // 别人发给我的
+            if (me.userId != mes.userId) { // 别人发给我的
+                let notPadding='' // 输入中的样式
+                if (text == '输入中...') {
+                    text = '<img  src="images/dot2.gif" alt="">' // 输入中动图
+                    notPadding = 'class="notPadding"'
+                }
                 html =
-                    '<span style="background:'+mes.bg+'">' +
-                    mes.word+
+                    '<span style="background:' + mes.bg + '">' +
+                    mes.word +
 //            '<img src="images/via'+mes.headImg+'.jpg" alt="">' +
                     '</span>' +
-                    '<p>' + text +
+                    '<p '+notPadding+'>' + text +
                     '</p>'
             }
             if (me.userId == mes.userId && !mes.sending) { // 我发给别人的
                 div.className = 'chat chatRight';
                 html =
-                    '<span style="background:'+mes.bg+'">' +
-                    mes.word+
+                    '<span style="background:' + mes.bg + '">' +
+                    mes.word +
 //                '<img src="images/via'+mes.headImg+'.jpg" alt="">' +
                     '</span>' +
-                    '<p>' +text +
+                    '<p>' + text +
                     '</p>'
             }
             div.innerHTML = html
             chatContent.appendChild(div)
             chatContent.scrollTop = chatContent.scrollHeight // 滚动条始终在底部
             me.lastDate = new Date()
-            /*if (mes.isInput){
-                var len = document.getElementsByClassName('chat').length
-                chatContent.removeChild(document.getElementsByClassName('chat')[len-1])
-            }*/
+            if (!mes.isInput && document.querySelector('.sending')) {
+                (chatContent.removeChild(document.querySelector('.sending')))
+            }
         });//
     }
 
